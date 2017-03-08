79148f011d2db7b98f7c2b942808d05855919a3c
David Esposito <david.esposito@bettercloud.com>
2017-03-08 13:31:53 -0500

    Adding Looksay solution
    Signed-off-by: David Esposito <david.esposito@bettercloud.com>

--- src/main/java/com/bettercloud/challenges/_2017/_03Mar/_030817_LookSay.java
+++ src/main/java/com/bettercloud/challenges/_2017/_03Mar/_030817_LookSay.java
@@ -1,5 +1,11 @@
 package com.bettercloud.challenges._2017._03Mar;
 
+import com.google.common.collect.Lists;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.stream.Collectors;
+
 /**
  * Created by davidesposito on 3/8/17.
  */
@@ -54,7 +60,37 @@ public class _030817_LookSay {
 
         @Override
         public String lookSay(String input, int iters) {
-            return null;
+            return iters == 0
+                    ? input
+                    : lookSay(
+                    Lists.newArrayList(input.split("")).stream()
+                            .map(c -> CharCount.builder().character(c).count(1).prefix("").build())
+                            .collect(Collectors.reducing(
+                                    (a, b) -> a.getCharacter().equals(b.getCharacter())
+                                            ? a.inc()
+                                            : b.merge(a)
+                            ))
+                            .map(cc -> cc.getPrefix() + cc.getCount() + cc.getCharacter())
+                            .orElse("")
+                    , iters - 1);
+        }
+    }
+
+    @Data
+    @Builder
+    private static class CharCount {
+        private String prefix;
+        private String character;
+        private int count;
+
+        public CharCount inc() {
+            this.count += 1;
+            return this;
+        }
+
+        public CharCount merge(CharCount other) {
+            this.prefix = other.getPrefix() + other.getCount() + other.getCharacter() + this.prefix;
+            return this;
         }
     }
 }
